3dac00ff4730ce73c71597528b7d76bbca0a2b4a
Márcio Krüger <marcio.kgr@gmail.com>
2022-06-24 17:54:45 -0300

    Add project files.

---
+++ GRPCClient/Program.cs
@@ -0,0 +1,10 @@
+﻿using Grpc.Net.Client;
+using ProjetoGrpc;
+
+var channel = GrpcChannel.ForAddress("https://localhost:7044");
+var client = new Greeter.GreeterClient(channel);
+
+var reply = await client.SayHelloAsync(
+     new HelloRequest { Name = "Márcio"});
+
+Console.WriteLine("Retorno do servidor: " + reply.Message);
